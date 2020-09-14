8483ccae71a217eb4cfa32efa7febf5c7030447a
Shan Eapen Koshy <shaneapen@gmail.com>
2020-09-14 10:45:16 +0530

    added build

---
+++ react-dashboard/build/precache-manifest.1db152511cfa87af292535fa60684e93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036657c5582da135fbde2b41cfc1661c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbe35099b4da4964900a",
+    "url": "/static/js/2.d2ea4a9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d2ea4a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c892d4e7396adccfdb28",
+    "url": "/static/js/main.2f7edbac.chunk.js"
+  },
+  {
+    "revision": "4040b55ff6b868e876aa",
+    "url": "/static/js/runtime-main.2c219ed3.js"
+  }
+]);
