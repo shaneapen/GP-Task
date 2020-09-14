debbf349e559befc813860bd134ecd6f417957be
Shan Eapen Koshy <shaneapen@gmail.com>
2020-09-14 14:13:35 +0530

    TESING5

--- core/views.py
+++ core/views.py
@@ -16,7 +16,8 @@ def home(request):
      currentUser = UserSocialAuth.objects.get(id = request.user.id)
      response.set_cookie('access_token', currentUser.extra_data['access_token'])
      response.set_cookie('uid', currentUser.uid)
-     response = render(request,"build/index.html")
+    #  response = render(request,"build/index.html")
+     response = HttpResponse('AUTHENTICATED Request User: ', request.user)
   else:
     response = HttpResponse('Request User: ', request.user)
 
